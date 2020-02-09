b96913c8cb3bf242b260c39619ba175296483c28
HyokaiShizuku <60824848+HyokaiShizuku@users.noreply.github.com>
2020-02-09 15:43:46 +0900

    Update Lead.java

--- src/Lead.java
+++ src/Lead.java
@@ -1,6 +1,7 @@
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -16,10 +17,10 @@ public class Lead extends JavaPlugin implements Listener{
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onLead(PlayerInteractEntityEvent event){
-		if(event.getPlayer().getItemInHand().getType() == Material.LEAD) {
+		if(event.getPlayer().getItemInHand().getType() == Material.LEAD && event.getRightClicked() instanceof Mob) {
 			LivingEntity le = (LivingEntity) event.getRightClicked();
 			le.setLeashHolder(event.getPlayer());
 			event.setCancelled(true);
 		}
 	}
-}
+}s
