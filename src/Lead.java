14bf173d82ab294d56c45ce38b6d4cb178aac552
HyokaiShizuku <60824848+HyokaiShizuku@users.noreply.github.com>
2020-02-09 14:39:41 +0900

    Add files via upload

---
+++ src/Lead.java
@@ -0,0 +1,25 @@
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lead extends JavaPlugin implements Listener{
+
+	@Override
+	public void onEnable() {
+		Bukkit.getConsoleSender().sendMessage("このプラグインを有効にしました!");
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onLead(PlayerInteractEntityEvent event){
+		if(event.getPlayer().getItemInHand().getType() == Material.LEAD) {
+			LivingEntity le = (LivingEntity) event.getRightClicked();
+			le.setLeashHolder(event.getPlayer());
+			event.setCancelled(true);
+		}
+	}
+}
